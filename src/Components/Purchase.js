312e4372f48e1e9c2fbf742cdbd9b73e7d0c59fd
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2021-12-02 08:43:53 +0100

    Began confirmation for purchase

--- src/Components/Purchase.js
+++ src/Components/Purchase.js
@@ -1,17 +1,65 @@
 import React, { useState, useEffect } from "react";
 import DashboardNav from "./DashboardNav";
 import {checkIsNumber} from "./Auth/Register";
+import { useToasts } from 'react-toast-notifications'
 
 export default function Purchase(){
+    const { removeAllToasts, addToast } = useToasts()
     const [amount, setAmount] = useState("")
     const [tryPayment, setTryPayment] = useState(false)
     const [isPayButtonDisabled, setPayButtonDisabled] = useState(false)
+    const [isBgOverlay, setBgOverlay] = useState(false)
     const attemptToPay = () => {
-        setTryPayment(true)
-        setPayButtonDisabled(true)
+        console.log(amount)
+        if(amount.length < 2){
+            //Amount is not up to 10
+            removeAllToasts()
+            addToast("Amount must be up to ₦10", {
+                appearance: "warning",
+                autoDismiss: true
+            })
+        }else{
+            setTryPayment(true)
+            setPayButtonDisabled(true)
+            setBgOverlay(true)
+        }
+    }
+    const confirmPinForPuchase = () => {
+        
     }
     return (
         <>
+            {
+                isBgOverlay && (
+                    <>
+                    <div className="bg-overlay"></div>
+                        <div className="add-money-dialog flex-column bg-white confirm-purchase-dialog">
+                            <div className="flex-row add-money-top">
+                                <span className="add-money-head rubik text-darker">
+                                    Confirm purchase
+                                </span>
+                                <span className="close-add-money flex-column text-darker"
+                                      onClick={() => {
+                                          setBgOverlay(false)
+                                          setTryPayment(false)
+                                          setPayButtonDisabled(false)
+                                      }}
+                                >
+                                <i className="far fa-times"></i>
+                            </span>
+                            </div>
+                            <p className="text-darker jost">
+                                Confirm payment of <b>₦{amount}</b>
+                            </p>
+                            <button className="auth-btn jost text-white bg-darker"
+                                onClick={confirmPinForPuchase}
+                            >
+                                Confirm
+                            </button>
+                        </div>
+                    </>
+                )
+            }
             <DashboardNav />
             <div className="purchase-container flex-column bg-white">
                 <span className="caterer-head text-darker jost">
